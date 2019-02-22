fb5c39c621ca5cb3ed737056e14e3a5296949a6d
sarkis74 <sarkis_aghazarian@yahoo.com>
2019-02-21 20:28:50 -0800

    starting travis ci

--- buffer_folder/__test__/loop.test.js
+++ buffer_folder/__test__/loop.test.js
@@ -16,8 +16,8 @@ describe('#loop  TDD testing for buffer functions', () => {
         let NewBuf = new Buf('John', 'Jane', 'Sue');
         let myArr = NewBuf.buffer([]);
         console.log(myArr.length);
-        expect(myArr.length).toEqual(3);
-        expect(Array.isArray(myArr)).toEqual(true);
+        expect(myArr.length).toEqual(3); //Currently array has 3 buffers
+        expect(Array.isArray(myArr)).toEqual(true); //Check if data type is array
         expect(typeof myArr.toString()).toEqual('string'); //Since fs write file converts buffers automatically to strings must add 'toString() to mimic fs method
     });
 });
