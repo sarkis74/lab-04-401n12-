ad6af5d8ff040f38feb14fb7a027ebc86a5a49f3
sarkis74 <sarkis_aghazarian@yahoo.com>
2019-02-21 20:08:14 -0800

    fs writes buffer into separate file, and tests passed

--- buffer_folder/__test__/loop.test.js
+++ buffer_folder/__test__/loop.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const Buf = require('../main');
+const Buf = require('../loop.js');
 
 describe('#loop  TDD testing for buffer functions', () => {
     beforeAll(() => {
@@ -15,7 +15,9 @@ describe('#loop  TDD testing for buffer functions', () => {
     it('Makes sure array length is 3 and data type is array', () => {
         let NewBuf = new Buf('John', 'Jane', 'Sue');
         let myArr = NewBuf.buffer([]);
+        console.log(myArr.length);
         expect(myArr.length).toEqual(3);
         expect(Array.isArray(myArr)).toEqual(true);
+        expect(typeof myArr.toString()).toEqual('string'); //Since fs write file converts buffers automatically to strings must add 'toString() to mimic fs method
     });
 });
