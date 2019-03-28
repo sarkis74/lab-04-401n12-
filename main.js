03dbae9c82014a4e43e23a388b2189a4e3f4649e
sarkis74 <sarkis_aghazarian@yahoo.com>
2019-03-27 19:28:04 -0700

    tests good now

--- main.js
+++ main.js
@@ -22,33 +22,10 @@ text.forEach(el => {
     console.log(el.toString());
 });
 
-
- // let content = fs.readFileSync('./test.txt',  "utf8", function(err, data){
- //     if (err) console.log(err);
- //     console.log("Successfully Written to File.");
- // });
- //
- // content = content.replace(/\.\s/g, '. •\n');
- // content = content.replace(/6 Reasons for Pair Programming/, '<article><h2>6 Reasons for Pair Programming<h2>');
- // content = content.replace(/How does pair programming work\?/, '<h2>How does pair programming work?<h2>');
- // content = content.replace(/Why pair program\?/, '<h2>Why pair program<h2>');
- // content = content.replace(/Wow, all that\? Let’s take a look\!/, '<h2>Wow, all that? Let’s take a look!<h2>');
- // content = content.replace(/1. Greater efficiency/, '<h3>1. Greater efficiency<h3>');
- // content = content.replace(/2. Engaged collaboration/, '<h3>2. Engaged collaboration<h3>');
- // content = content.replace(/3. Learning from fellow students/, '<h3>3. Learning from fellow students<h3>');
- // content = content.replace(/4. Social skills/, '<h3>4. Social skills<h3>');
- // content = content.replace(/5. Job interview readiness/, '<h3>5. Job interview readiness<h3>');
- // content = content.replace(/6. Work environment readiness/, '<h3>6. Work environment readiness<h3>');
- // content = content.replace(/while developing key industry skills\./, 'while developing key industry skills.<article>');
- //
  fs.writeFile(`${__dirname}/loop.js`, text, function(err, data){
      if (err) console.log(err);
      console.log("Successfully Written to File.");
  });
-
-
- // let buf = new Buffer('•');
-
 
  module.exports = Buf;
 
